e8c59a87ce633460c25b7a63899f60d7c1f83aed
djangonasarudheen <105368970+djangonasarudheen@users.noreply.github.com>
2022-05-11 21:10:34 +0300

    Delete orginal directory

--- orginal/SelfBlog/Blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import Comment
-
-class CommentForm(forms.ModelForm): #to generates forms.py based on database model
-    class Meta:
-        model = Comment
-        fields = ['name','email','body'] #date_added automaticaly add
