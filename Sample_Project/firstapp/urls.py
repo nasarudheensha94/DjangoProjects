356805b014c45a98b150aaca4499545ebe1b0969
djangonasarudheen <105368970+djangonasarudheen@users.noreply.github.com>
2022-05-11 18:50:25 +0300

    Delete Sample_Project directory

--- Sample_Project/firstapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= {
-    path('', views.home, name="home"),
-    path('index', views.index, name="index"),
-    path('About', views.About, name="About"),
-
-}
